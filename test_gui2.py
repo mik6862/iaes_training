4db47e4638f85e1b6583f31cb7e918ae12452e29
Konstantin <kostua16@gmail.com>
2013-07-01 19:52:39 +0400

    added test_gui2.py

---
+++ test_gui2.py
@@ -0,0 +1,54 @@
+__author__ = 'kostua'
+
+from traits.api import HasTraits, Float, Button
+from traitsui.api import Item, Group, View, HGroup, VGroup
+
+
+class test_gui(HasTraits):
+    a = Float()
+    b = Float()
+    r = Float()
+    plus = Button(label='+')
+    minus = Button(label='-')
+    multi = Button(label='*')
+    sub = Button(label='/')
+
+    values = VGroup(Item('a', label='First'),
+                    Item('b', label='Second'),
+                    Item('r', label='Result'),
+                    label='Data',
+                    show_border=True)
+
+    buttons = HGroup(Item('plus', show_label=False),
+                     Item('minus', show_label=False),
+                     Item('multi', show_label=False),
+                     Item('sub', show_label=False),
+                     label='Buttons',
+                     show_border=True)
+
+    main_gr = Group(values, buttons)
+
+    view = View(main_gr,
+                title='Simple calulator')
+
+    def _plus_fired(self):
+        self.r = self.a + self.b
+
+    def _minus_fired(self):
+        self.r = self.a - self.b
+
+    def _multi_fired(self):
+        self.r = self.a * self.b
+
+    def _sub_fired(self):
+        try:
+            self.r = self.a / self.b
+        except Exception:
+            self.r = 0.0
+
+    def __init__(self):
+        self.configure_traits()
+
+
+if __name__ == "__main__":
+    test_gui()
